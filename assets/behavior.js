0b82444d52f8237407370a8e555d8dd9bf3c5833
sonnym <michaud.sonny@gmail.com>
2012-07-24 00:46:30 -0400

    Generated documentation for 1218e453feaaee9c334ad8b14fad4fdcd482358a

--- assets/behavior.js
+++ assets/behavior.js
@@ -399,6 +399,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                       "type": "heading",
                       "data": {
                         "level": 3,
+                        "title": "comment",
+                        "slug": "comment"
+                      },
+                      "depth": 3
+                    }, {
+                      "type": "heading",
+                      "data": {
+                        "level": 3,
                         "title": "schedule",
                         "slug": "schedule"
                       },
@@ -567,6 +575,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                     "level": 3,
                     "title": "estimate",
                     "slug": "estimate"
+                  },
+                  "depth": 3
+                }, {
+                  "type": "heading",
+                  "data": {
+                    "level": 3,
+                    "title": "comment",
+                    "slug": "comment"
                   },
                   "depth": 3
                 }, {
